94208aff2eccc64567ea6ad102cf0b2a0f5d609b
Ujjwal Sharma <usharma1998@gmail.com>
2019-10-27 03:06:54 +0530

    M A G I C

--- index.mjs
+++ index.mjs
@@ -4,33 +4,37 @@ const [
   host = "0.0.0.0",
   port = 1337,
   data = "hello",
-  count = 100
+  numClients = 5,
+  count = 200
 ] = process.argv.slice(2);
 
-const requests = [];
-const start = Date.now();
+(async () => {
+  const start = Date.now();
+  for (let i = 0; i < numClients; i++) {
+    await makeClient(data, count);
+  }
+  console.log(Date.now() - start);
+})();
 
-for (let i = 0; i < count; i++) {
-  const req = new Promise((resolve, reject) => {
-    const client = new net.Socket();
-    client.connect(port, host, () => {
-      client.write(data);
+function makeRequest(client, data) {
+  return new Promise(resolve => {
+    client.write(data);
+    client.on("data", res => {
+      if (res.toString() === data) resolve();
     });
-    client.on("data", () => {
+  });
+}
+
+function makeClient(data, count) {
+  return new Promise(resolve => {
+    const client = new net.Socket();
+    client.setMaxListeners(count);
+    client.connect(port, host, async () => {
+      for (let i = 0; i < count; i++) {
+        await makeRequest(client, data + i);
+      }
       client.destroy();
       resolve();
     });
-    setTimeout(() => {
-      reject("request timed out");
-    }, 1000);
   });
-  requests.push(req);
 }
-
-Promise.all(requests)
-  .then(() => {
-    console.log(Date.now() - start);
-  })
-  .catch(e => {
-    console.error(e);
-  });
